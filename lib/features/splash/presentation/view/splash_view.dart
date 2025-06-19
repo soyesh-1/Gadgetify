0b25e5783495995018a169633ddabce2dec02f7d
soyesh-1 <soyeshxrestha@gmail.com>
2025-06-19 09:44:06 +0545

    changing the ui of the splash View in view

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -17,7 +17,32 @@ class _SplashViewState extends State<SplashView> {
     // `listen: false` is important because we don't need to rebuild this widget
     // when the ViewModel changes. We just need to call a method.
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<SplashViewModel>(context, listen: false).checkLoginStatus(context);
+      Provider.of<SplashViewModel>(
+        context,
+        listen: false,
+      ).checkLoginStatus(context);
     });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    // This is the UI part of your original code.
+    return Scaffold(
+      backgroundColor: const Color(0xFF7E57C2), // Or your primary app color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // IMPORTANT: Make sure you have this image in your assets folder.
+            Image.asset("assets/images/gadgetify_logo.png", height: 250),
+            const SizedBox(height: 30),
+            const CircularProgressIndicator(
+              color: Colors.white,
+              strokeWidth: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
