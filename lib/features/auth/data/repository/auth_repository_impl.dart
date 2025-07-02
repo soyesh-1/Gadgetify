696f7275c811d554dcfca227af166b8788c2a5ae
soyesh-1 <soyeshxrestha@gmail.com>
2025-07-02 17:29:22 +0545

    Changing the UI

--- lib/features/auth/data/repository/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:gadgetify/core/error/failure.dart';
-import 'package:gadgetify/core/network/network_info.dart'; // Import NetworkInfo
+import 'package:gadgetify/core/network/network_info.dart';
 import 'package:gadgetify/features/auth/data/data_source/local_datasource/auth_local_data_source.dart';
-import 'package:gadgetify/features/auth/data/data_source/remote_datasource/auth_remote_data_source.dart'; // Import Remote Data Source
+import 'package:gadgetify/features/auth/data/data_source/remote_datasource/auth_remote_data_source.dart';
 import 'package:gadgetify/features/auth/data/model/auth_hive_model.dart';
 import 'package:gadgetify/features/auth/domain/entity/auth_entity.dart';
 import 'package:gadgetify/features/auth/domain/repository/auth_repository.dart';
@@ -20,12 +20,9 @@ class AuthRepositoryImpl implements IAuthRepository {
 
   @override
   Future<Either<Failure, void>> signup({required AuthEntity user}) async {
-    // Always sign up via the API.
     if (await _networkInfo.isConnected) {
       try {
-        // Call the API to register the user remotely.
         await _remoteDataSource.signup(user);
-        // If the API call is successful, also save the user to the local Hive database.
         final authHiveModel = AuthHiveModel.fromEntity(user);
         await _localDataSource.signup(authHiveModel);
         return const Right(null);
@@ -44,11 +41,8 @@ class AuthRepositoryImpl implements IAuthRepository {
   }) async {
     if (await _networkInfo.isConnected) {
       try {
-        // Try to log in using the API.
         final isSuccess = await _remoteDataSource.login(email, password);
         if (isSuccess) {
-          // If remote login is successful, you could cache user data/token here.
-          // For now, we just return success.
           return const Right(true);
         } else {
           return Left(Failure(error: 'Invalid credentials from API.'));
@@ -57,11 +51,9 @@ class AuthRepositoryImpl implements IAuthRepository {
         return Left(Failure(error: e.toString()));
       }
     } else {
-      // If OFFLINE, try to log in using the local Hive database.
       try {
         final user = await _localDataSource.login(email, password);
         if (user != null) {
-          // Offline login successful
           return const Right(true);
         } else {
           return Left(Failure(error: 'Invalid credentials or no internet.'));
