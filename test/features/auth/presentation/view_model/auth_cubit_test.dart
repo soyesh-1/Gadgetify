2411dd0225d23259e34a7c19dd7218144c6192d4
soyesh-1 <soyeshxrestha@gmail.com>
2025-07-13 16:58:20 +0545

    Adding the auth cubit  testing

--- test/features/auth/presentation/view_model/auth_cubit_test.dart
+++ test/features/auth/presentation/view_model/auth_cubit_test.dart
@@ -1,7 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gadgetify/core/error/failure.dart';
 import 'package:gadgetify/features/auth/domain/entity/auth_entity.dart';
 import 'package:gadgetify/features/auth/domain/use_case/login_use_case.dart';
 import 'package:gadgetify/features/auth/domain/use_case/signup_use_case.dart';
